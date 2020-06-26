0bedde75abe4d87d3ae239ce7245f22934226909
Reeti Bhagat <bhagat.reeti@gmail.com>
2020-06-25 23:25:30 -0700

    Initial files commit for this project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction of price for big mountain resort to increase revenue',
+    author='Reeti Bhagat',
+    license='MIT',
+)
---
+++ src/funct/func.py
@@ -0,0 +1,5 @@
+def foo(x):
+    if x is 'foo':
+        return 'bar'
+    else:
+        return 'baz'
